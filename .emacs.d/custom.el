167d59d5c8de891139c1dbcd7fdfbc685eaf1302
Nikolay Amiantov <ab@fmap.me>
2014-10-15 00:54:49 +0400

    emacs: default font

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -46,4 +46,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Liberation Mono" :foundry "unknown" :slant normal :weight normal :height 98 :width normal)))))
