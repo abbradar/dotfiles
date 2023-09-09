f24e372ac986185d233e2241ffcb4db151e97a92
Nikolay Amiantov <ab@fmap.me>
2023-09-09 11:26:58 +0700

    Fix the typo

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
 
   outputs = { self, emacs-overlay }: {
     nixosModules.common = import ./configuration-common.nix;
-    nixosModules.workspace.imports = [
-      ./configuration-workspace.nix
+    nixosModules.workplace.imports = [
+      ./configuration-workplace.nix
       { nixpkgs.overlays = [ emacs-overlay.overlays.default ]; }
     ];
     nixosModules.no-graphics = import ./configuration-no-graphics.nix;
