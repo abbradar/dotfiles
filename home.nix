72a7219b207b842928ff21a237d6efb95e40678e
Nikolay Amiantov <ab@fmap.me>
2025-02-18 00:35:05 +0700

    Fix coq_nvim

--- home.nix
+++ home.nix
@@ -53,12 +53,14 @@ with lib; {
     vimdiffAlias = true;
     plugins = with pkgs.vimPlugins; [
       lazy-nvim
+      coq_nvim
     ];
     # https://github.com/LazyVim/LazyVim/discussions/1972
     extraLuaConfig = let
       plugins = with pkgs.vimPlugins; [
         nvim-lspconfig
         nvim-treesitter.withAllGrammars
+        coq_nvim
       ];
       mkEntryFromDrv = drv:
         if lib.isDerivation drv
